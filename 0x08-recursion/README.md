ef449e598f05fdf306d43843b8a57d6f0bc49717
ComfortUdoakaha <comfortudoakaha64@gmail.com>
2023-04-05 10:20:31 -0700

    Tasks

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This file shows the project 0x08-recursion
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _putchar(char c);
