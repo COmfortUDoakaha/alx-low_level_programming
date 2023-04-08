9a09a9ccec95f70942467095fda628be53a4f3af
ComfortUdoakaha <comfortudoakaha64@gmail.com>
2023-04-07 22:46:10 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit of base
+ * 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchat('\n');
+	return (0);
+}
